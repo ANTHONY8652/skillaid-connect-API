3704519a025aeea40e90fe1872866db51e46b8f1
ANTHONY8652 <githinjianthony720@gmail.com>
2024-11-05 13:49:02 +0300

    users and courses

--- course/views.py
+++ course/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from .models import Course
+from .serializers import CourseSerializer
+
+class CourseListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CourseRetrieveUpdateOrDestroyView(generics.RetrieveDestroyAPIView):
+    permission_classes = [permissions.IsAdminUser]
 
 # Create your views here.
--- lesson/views.py
+++ lesson/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from course.models import Course
+from rest_framework import generics, status, permissions
+from .models import Lesson
+from .serializers import LessonSerializer
+
+class LessonListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LessonSerializer
+
+    def get_queryset(self, request):
+        course_id = self.kwargs['course_id']
+        return Lesson.objects.filter(course__id=course_id)
+    
+    def perform_create(self, serializer):
+        course_id = self.kwargs['course_id']
+        course = Course.objects.get(id=course_id)
+        serializer.save(course=course)
 
 # Create your views here.
